fa88910281d882b2aa96c7bbc64c9f58f99de313
tarian <70381096+4masa6@users.noreply.github.com>
2022-04-10 14:58:49 +0900

    JSX記法

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+// JSX記法：JavaScriptの中でHTMLのタグをreturnする
+// returnする中のタグは、１つのタグで囲う必要がある <>~</>（<React.Fragment>~</React.Fragment>の略）
 const App = () => {
-  return null;
+  return (
+    <>
+      <h1>こんにちは</h1>
+      <p>お元気ですか</p>
+    </>
+  );
 };
 
 ReactDom.render(<App />, document.getElementById("root"));
