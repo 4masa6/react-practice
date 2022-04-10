c6de8faad2a771e3d454242df3fb990920e7a4ff
tarian <70381096+4masa6@users.noreply.github.com>
2022-04-10 15:05:00 +0900

    コンポーネントの使い方

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-// JSX記法：JavaScriptの中でHTMLのタグをreturnする
-// returnする中のタグは、１つのタグで囲う必要がある <>~</>（<React.Fragment>~</React.Fragment>の略）
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか</p>
-    </>
-  );
-};
+import App from "./App"; // Appコンポーネントをインポート
 
 ReactDom.render(<App />, document.getElementById("root"));
