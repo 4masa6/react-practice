0a177a9aa33f7f86d3b4802cdfc853609ec7ef05
tarian <70381096+4masa6@users.noreply.github.com>
2022-04-10 15:16:51 +0900

    ReactでのイベントやStyleの扱い方

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,19 @@ import React from "react";
 // JSX記法：JavaScriptの中でHTMLのタグをreturnする
 // returnする中のタグは、１つのタグで囲う必要がある <>~</>（<React.Fragment>~</React.Fragment>の略）
 const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "red",
+    fontSize: "18px"
+  };
+
   return (
     <>
-      <h1>こんにちは</h1>
+      {/* JSXではJavaScriptの属性をキャメルケースで書く */}
+      {/* HTMLの中にJavaScriptを書く時は{}で囲う */}
+      <h1 style={contentStyle}>こんにちは</h1>
       <p>お元気ですか</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
